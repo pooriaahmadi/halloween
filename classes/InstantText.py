72906cb6ff61d9f8495120ccbad2a853b11221fd
pooriaahmadi <pooriababak444@gmail.com>
2022-10-12 09:56:13 -0400

    instant text

---
+++ classes/InstantText.py
@@ -0,0 +1,34 @@
+from .ScrollingText import ScrollingText
+
+class InstantText(ScrollingText):
+    words = []
+    def __init__(self, width: int, height: int, wait: int = 3, wipe: bool = False):
+        super().__init__(width, height, wait, wipe)
+        self.set_parameter("text", "hi 5")
+
+    def set_parameter(self, key: str, value):
+        if key == "color" and not isinstance(value, int):
+            self.parameters[key] = Color(
+                int(value[0]), int(value[1]), int(value[2]))
+            for letter in self.LETTERS.values():
+                letter.change_color(self.parameters[key])
+            return
+        if key == "text":
+            self.words = value.split(" ")
+        self.parameters[key] = value
+
+    def step(self):
+        self.current_frame.pixels = []
+        if self.index >= len(words):
+            self.index = 0
+
+        for index, character in enumerate(self.words[self.index]):
+            if character == " ":
+                frame = Frame.empty(8, 8)
+            else:
+                frame = self.LETTERS[character]
+
+            self.current_frame.draw_frame_at_pos(
+                frame, index * 5, 2)
+
+        self.index += 1
